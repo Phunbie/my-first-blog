4cac372a972f21482381dcf63fef75944cfc6c43
Phunbie <adeneyeoluwafunbi@gmail.com>
2022-11-03 00:15:30 +0100

    create login view and page

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_home, name='post_home'),
-    #path('<str:Author.username>/', views.post_list, name='post_list'),
+    #path('<str:username>', views.post_list, name='post_list_test'),
     path('post/user/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,14 @@ from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your views here.
 def post_home(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_home.html', {'posts':posts})
 
-@login_required
+#@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now(), author=request.user).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
