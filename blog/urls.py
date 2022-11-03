7ce110e226c5eee8fe6e92c80682cf61c71f1c4a
Phunbie <adeneyeoluwafunbi@gmail.com>
2022-11-03 02:22:49 +0100

    create full CRUD version

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     #path('<str:username>', views.post_list, name='post_list_test'),
     path('post/user/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/noedit/<int:pk>/', views.post_detail_noedit, name='post_detail_noedit'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
