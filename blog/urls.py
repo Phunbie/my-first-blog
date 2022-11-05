f6910fb5b288cab7a51fd7beac2674c87667c541
Phunbie <adeneyeoluwafunbi@gmail.com>
2022-11-05 09:06:12 +0100

    add delete post option

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('post/noedit/<int:pk>/', views.post_detail_noedit, name='post_detail_noedit'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import redirect
+from django.shortcuts import redirect, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 # Create your views here.
 def post_home(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_home.html', {'posts':posts})
 
 @login_required
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now(), author=request.user).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now(), author=request.user).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 @login_required
@@ -53,3 +53,12 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return HttpResponseRedirect('/post/user')
+    return render(request, 'blog/delete.html', {'post':post})
+
